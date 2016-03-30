8917ca135f1b2c1a593639e07c21837f3963be58
Colin Baxter <colin.baxter@zaneray.com>
2016-03-30 16:17:27 -0600

    First round of development of full style guide, styling, theme toggle, etc

--- js/outdoor-research.js
+++ js/outdoor-research.js
@@ -1,2 +1,13 @@
-//some JS will be here
-console.log("hey whats happening?");
+$(function(){
+  //Theme toggle click event
+  var $themeToggle = $('.js-theme-toggle'),
+      $interfaceElementContainer = $('#interface-elements');
+
+  $themeToggle.on('click', function(){
+    console.log("hey");
+    $themeToggle.toggleClass("dark-theme");
+    $interfaceElementContainer.toggleClass("dark-theme");
+  });
+
+});
+
