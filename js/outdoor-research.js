a78004143071b94a6ee33446a152b1da960bfc1f
Colin Baxter <colin.baxter@zaneray.com>
2016-04-29 12:00:35 -0600

    Adding in iconography section

--- js/outdoor-research.js
+++ js/outdoor-research.js
@@ -8,5 +8,28 @@ $(function(){
     $interfaceElementContainer.toggleClass("dark-theme");
   });
 
+  var $styleGuideAnchorNavSpan = $('.style-guide-anchor-span');
+  $styleGuideAnchorNavSpan.on('click',function(e){ 
+    e.preventDefault();
+    $('.style-guide-anchor-nav').toggleClass('active');
+  }); 
+
+
+  $('.style-guide-anchor-nav').on('click touchstart',function(e){
+    e.stopPropagation();
+  })
+
+  $('body').on('click touchstart',function() {
+    $('.style-guide-anchor-nav').removeClass('active');
+  })
+
+  var $styleGuideAnchorList = $('#style-guide-anchor-list');
+  //set up the anchor nav links
+  $('.style-guide-section').each(function(){
+    var id = this.id,
+        title = $(this).children('.section-header').text();
+    $styleGuideAnchorList.append('<li class="style-guide-anchor-item"><a href="#' + id + '" class="style-guide-anchor-link">' + title + '</a></li>')
+  });
+
 });
 
